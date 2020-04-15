ef8c9092216a0b6a9f989433d18b873af8351665
PJRaj <vijayrajshanmugarajt@gmail.com>
2020-04-16 01:00:34 +0530

    wdijd

--- covid.py
+++ covid.py
@@ -44,38 +44,63 @@ x_scaled = min_max_scaler.fit_transform(x)
 train_df = pd.DataFrame(x_scaled)
 
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-mae_a = []
-mse_a = []
-err_A = []
+# mae_a = []
+# mse_a = []
+# err_A = []
 
-for i in range(0,20):
+# for i in range(0,20):
 
-    X_train, X_test, y_train, y_test = train_test_split(train_df, y, test_size=.2, random_state=i)
+#     X_train, X_test, y_train, y_test = train_test_split(train_df, y, test_size=.2, random_state=i)
 
-    from sklearn.linear_model import LinearRegression
-    reg = LinearRegression().fit(np.array(X_train), np.array(y_train))
-    y_e = reg.predict(X_test)
+#     reg = LinearRegression().fit(np.array(X_train), np.array(y_train))
+#     y_e = reg.predict(X_test)
 
-    # from sklearn import linear_model
-    # reg = linear_model.BayesianRidge(n_iter = 1000)
-    # reg.fit(np.array(X_train), np.array(y_train))
-    # y_e = reg.predict(X_test)
 
-    from sklearn.metrics import mean_absolute_error, mean_squared_error
+#     from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-    mse = mean_squared_error(y_e,y_test)
-    mae = mean_absolute_error(y_e,y_test)
+#     mse = mean_squared_error(y_e,y_test)
+#     mae = mean_absolute_error(y_e,y_test)
 
-    err = [abs((y_e[i]-y_test[i])/y_test[i]) for i in range(len(y_e))]
-    e_m = sum(err)/len(err)
-    mae_a.append(mae)
-    mse_a.append(mse)
-    err_A.append(e_m)
+#     err = [abs((y_e[i]-y_test[i])/y_test[i]) for i in range(len(y_e))]
+#     e_m = sum(err)/len(err)
+#     mae_a.append(mae)
+#     mse_a.append(mse)
+#     err_A.append(e_m)
 
-print(mae_a)
-print(mse_a)
-print(err_A)
+# print(mae_a)
+# print(mse_a)
+# print(err_A)
 
-print(np.mean(mae_a),np.mean(mse_a),np.mean(err_A))
+# print(np.mean(mae_a),np.mean(mse_a),np.mean(err_A))
 # print(train_df)
+
+regg = LinearRegression().fit(np.array(train_df), np.array(y))
+
+df1= pd.read_csv('Datasets/COVID/test.csv', names=['smiles'], skiprows = 1)
+u = list(df1['smiles'].values)
+
+df1['mol'] = df1['smiles'].apply(lambda x: Chem.MolFromSmiles(x)) 
+df1['mol'] = df1['mol'].apply(lambda x: Chem.AddHs(x))
+
+df1['num_of_atoms'] = df1['mol'].apply(lambda x: x.GetNumAtoms())
+df1['num_of_bonds'] = df1['mol'].apply(lambda x: x.GetNumBonds())
+df1['num_of_bonds_sq'] = df1['mol'].apply(lambda x: x.GetNumBonds()**2)
+
+# df['ringsar'] = df['mol'].apply(lambda x: Des.CalcNumAromaticRings(x))
+df1['rings_sq'] = df1['mol'].apply(lambda x: Des.CalcNumAromaticRings(x)**2)
+
+# df['num_of_heavy_atoms'] = df['mol'].apply(lambda x: x.GetNumHeavyAtoms())
+df1['n_h_a_sq'] = df1['mol'].apply(lambda x: x.GetNumHeavyAtoms()**2)
+
+def number_of_atoms(atom_list, df):
+    for i in atom_list:
+        df1['num_of_{}_atoms'.format(i)] = df1['mol'].apply(lambda x: len(x.GetSubstructMatches(Chem.MolFromSmiles(i))))
+
+number_of_atoms(['C','O','N'], df1)
+df1 = df1.drop(columns=['smiles', 'mol'])
+
+y_e = regg.predict(min_max_scaler.transform(np.array(df1)))
+# print(u[0])
+print('\n'.join([str(u[i]) + ','+str(y_e[i]) for i in range(len(y_e))]))
--- hate.py
+++ hate.py
@@ -1,24 +1,65 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.feature_extraction import TfidfVectoriser
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
 
 def preprocess(x):
     def deEmojify(inputString):
         return inputString.encode('ascii', 'ignore').decode('ascii')
 
+    x = x.replace('@', '')
+    x = re.sub(r'^https?:\/\/.*[\r\n]*', '', x)
+    x = x.lower()
     return deEmojify(x)
 # v = "Happy #JohnMCainDay #JohnMcCainDayJune14th #JohnMcCainAmericanHero 🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸🇺🇸Also #FuckTrump"
-print(preprocess(v))
+# print(preprocess(v))
 
-df= pd.read_csv('Datasets/COVID/train.csv')
+df= pd.read_csv('Datasets/HATE/train.csv')
+# print(df)
+df['text'] = df['text'].apply(lambda x: preprocess(x))
+tfidf = TfidfVectorizer(min_df=1, analyzer='word', ngram_range=(1, 2), stop_words = 'english')
 
-tfidf = TfidfVectorizer(min_df=1, analyzer='word',token_pattern=regex,
-            ngram_range=(1, 3), sublinear_tf=True, stop_words = 'english')
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
-X_train, X_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=.2, random_state=i)
+clf = SVC(kernel = 'linear', gamma='auto')
+# clf = LinearSVC(random_state=0, tol=1e-5, C = 0.1)
+# clf = RandomForestClassifier(max_depth=5, n_estimators=50, max_features=3)
 
-tfidf_inter = tfidf.fit_transform(X_train)
+# clf = AdaBoostClassifier(n_estimators=50)
+# clf = LogisticRegression(random_state=0, C=10)
+a_a = []
+f_a = []
 
-tfidf.transform(X_test)
+# for i in range(10):
+#     X_train, X_test, y_train, y_test = train_test_split(df['text'], df['labels'], test_size=.2, random_state=i)
 
+#     X_t = tfidf.fit_transform(X_train)
+#     clf.fit(X_t, y_train)
+
+#     Data_test = tfidf.transform(X_test)
+#     pred = clf.predict(Data_test)
+
+#     from sklearn.metrics import f1_score, accuracy_score
+#     a = accuracy_score(pred, y_test)
+#     f = f1_score(pred, y_test)
+#     a_a.append(a)
+#     f_a.append(f)
+
+X_t = tfidf.fit_transform(df['text'])
+clf.fit(X_t, df['labels'])
+
+df1= pd.read_csv('Datasets/HATE/test.csv')
+df1['text'] = df1['text'].apply(lambda x: preprocess(x))
+d_test = tfidf.transform(df1['text'])
+pred = clf.predict(d_test)
+
+print('labels')
+print('\n'.join(str(u) for u in pred))
+
+# print(np.mean(a_a),np.mean(f_a))
